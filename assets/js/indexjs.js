223526ee62a2dc2e7870818fb43b2aa7d36f5fa5
Yuchendou <9.yuchen@gmail.com>
2019-12-13 16:38:11 +0800

    god
    w

--- assets/js/indexjs.js
+++ assets/js/indexjs.js
@@ -1,27 +1,100 @@
 
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+var slideindex,slides,dots,captiontext;
+function initgallery(){
+  slideindex=0;
+  slides=document.getElementsByClassName("imageholder");
+  slides[slideindex].style.opacity=1;
+
+  captiontext=document.querySelector(".captionholder .captiontext")
+  captiontext.innerText=slides[slideindex].querySelector(".captiontext").innerText;
+
+  dots=[];
+  var dotscontainer=document.getElementById("dotscontainer");
+
+  for(var i=0; i<slides.length;i++)
+  {
+    var dot=document.createElement("span");
+    dot.classList.add("dots");
+    
+    dotscontainer.append(dot);
+    dots.push(dot);
+  }
+  dots[slideindex].classList.add("active");
+
+}
+initgallery();
+function plusSlides(n)
+{
+  moveslide(slideindex+n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+function moveslide(n)
+{
+  var i,current,next;
+  var moveslideanimclass={
+    forcurrent:"",
+    fornext:""
+  }
+  var slidetextanimclass;
+  if(n>slideindex)
+  {
+    if(n>=slides.length){n=0}
+    moveslideanimclass.forcurrent="moveleftcurrentslide";
+    moveslideanimclass.fornext="moveleftnextslide";
+    slidetextanimclass="slidetextfromtop";
+  }
+  else if(n<slideindex)
+  {
+    if(n<0){
+      n=slides.length-1
+    }
+    moveslideanimclass.forcurrent="moverighttcurrentslide";
+    moveslideanimclass.fornext="moverightnextslide";  
+    slidetextanimclass="slidetextfrombutton";
+  }
+  if(n!=slideindex)
+  {
+    next=slides[n];
+    current=slides[slideindex];
+    for(i=0;i<slides.length;i++)
+    {
+      slides[i].className="imageholder";
+      slides[i].style.opacity=0;
+      dots[i].classList.remove("active");
+    }
+    current.classList.add(moveslideanimclass.forcurrent);
+    next.classList.add(moveslideanimclass.fornext);
+    dots[n].classList.add("active");
+    slideindex=n;
+  }
+  captiontext.style.display="none";
+  captiontext.className="captiontext" + slidetextanimclass;
+  captiontext.innerText= slides[n].querySelector(".captiontext").innerText;
+  captiontext.style.display="block";
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+
+}
+var timer=null;
+function settimer()
+{
+  timer=setInterval(function()
+    {plusSlides(1);}
+    ,3000)
+}  
+settimer();
+
+function playpauseslide()
+{
+  var playpbutton=document.getElementById("playpbutton");
+  if(timer==null)
+  {
+    settimer();
+    playpbutton.style.backgroundPosition="0px";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+  else{
+    clearInterval(timer);
+    timer=null;
+    playpbutton.style.backgroundPositionY="-33px";
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
 }
